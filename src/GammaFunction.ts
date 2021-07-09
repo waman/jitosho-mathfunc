a0400bc3508cfb19f20c331d6f64ed96a6acf59f
waman <waman10da@gmail.com>
2021-07-10 05:36:24 +0900

    Add gamma function

--- src/GammaFunction.ts
+++ src/GammaFunction.ts
@@ -1,9 +1,10 @@
-const LOG_2PI = Math.log(2 * Math.PI);
+/** (1/2)log(2π) */
+const LOG_2PI_BY2 = Math.log(2 * Math.PI) / 2;
 const N = 8;
 
 // Bernoulli numbers
-const B0 = 1;
-const B1 = -1/2;
+// const B0 = 1;
+// const B1 = -1/2;
 const B2 = 1/6;
 const B4 = -1/30;
 const B6 = 1/42;
@@ -13,26 +14,39 @@ const B12 = -691/2730;
 const B14 = 7/6;
 const B16 = -3617/510;
 
+/**
+ * Return a logarithm value of the gamma function.
+ * 
+ * Ref: 『Javaによるアルゴリズム事典』ガンマ関数 (gamma function) Gamma.java
+ * 　　　『改訂新版 Cによる標準アルゴリズム事典』ガンマ関数 (gamma function) gamma.c
+ */
 export function logGamma(x: number): number {
     let v = 1;
     while(x < N){ v *= x; x++; }
-    let w = 1/(x*x);
-    let ret =       B16 / (16 * 15);
-    ret = ret * w + B14 / (14 * 13);
-    ret = ret * w + B12 / (12 * 11);
-    ret = ret * w + B10 / (10 *  9);
-    ret = ret * w + B8  / ( 8 *  7);
-    ret = ret * w + B6  / ( 6 *  5);
-    ret = ret * w + B4  / ( 4 *  3);
-    ret = ret * w + B2  / ( 2 *  1);
-    ret = ret / x + 0.5 * LOG_2PI - Math.log(v) - x + (x - 0.5) * Math.log(x);
-    return ret;
+    const w = 1/(x*x);
+    return ((((((((B16 / (16 * 15))  * w + (B14 / (14 * 13))) * w
+                + (B12 / (12 * 11))) * w + (B10 / (10 *  9))) * w
+                + (B8  / ( 8 *  7))) * w + (B6  / ( 6 *  5))) * w
+                + (B4  / ( 4 *  3))) * w + (B2  / ( 2 *  1))) / x
+                + LOG_2PI_BY2 - Math.log(v) - x + (x - 0.5) * Math.log(x);
+    // let ret =       B16 / (16 * 15);
+    // ret = ret * w + B14 / (14 * 13);
+    // ret = ret * w + B12 / (12 * 11);
+    // ret = ret * w + B10 / (10 *  9);
+    // ret = ret * w + B8  / ( 8 *  7);
+    // ret = ret * w + B6  / ( 6 *  5);
+    // ret = ret * w + B4  / ( 4 *  3);
+    // ret = ret * w + B2  / ( 2 *  1);
+    // ret = ret / x + 0.5 * LOG_2PI - Math.log(v) - x + (x - 0.5) * Math.log(x);
+    // return ret;
 }
 
+/**
+ * Return a value of the gamma function.
+ */
 export function gamma(x: number): number {
-    if(x < 0){
+    if(x < 0)
         return Math.PI / (Math.sin(Math.PI * x) * Math.exp(logGamma(1 - x)));
-    }else{
+    else
         return Math.exp(logGamma(x));
-    }
 }
--- src/chart.spec.ts
+++ src/chart.spec.ts
@@ -1,41 +1,42 @@
 import * as fs from 'fs';
-import { Distribution } from './Distribution';
-import { NormalDistribution } from './NormalDistribution';
+import { gamma } from './GammaFunction'
 
-describe('Output pdf', () => {
-    it('triangular distribution', () => {
-        outputPdf('./dist/pdf.html', NormalDistribution.create(), -1, 6);
-    });
+it('Output function graph', () => {
+    outputChart('./dist/chart.html', 'Gamma function', gamma, -5, 5);
 });
 
-function outputPdf(file: string, dist: Distribution,
-                    min: number = dist.min() !== Number.NEGATIVE_INFINITY ? dist.min() : -1,
-                    max: number = dist.max() !== Number.POSITIVE_INFINITY ? dist.max() : 1){
-    const n = 100;
-    const delta = (max - min)/n;
-    const data = new Array<{x: number, y: number}>();
-    for(let x = min; x < max; x+=delta){
-        data.push({x: x, y: dist.pdf(x)});
+function outputChart(file: string, name: string, f: (x: number) => number,
+        xMin = -1, xMax = 1, yMin = -10, yMax = 10){
+    const n = 10000;
+    const delta = (xMax - xMin)/n;
+    const data = new Array<{x: number, y: number|null}>();
+    for(let x = xMin; x < xMax; x+=delta){
+        const y = f(x);
+        const yy = yMin <= y && y <= yMax ? y : null;
+        data.push({x: x, y: yy});
     }
+
+    const labelsStr = data.map(d => `"${Math.floor(d.x*n)/n}"`).join(',');
+    const dataStr = data.map(d => d.y !== null ? d.y.toString() : 'null').join(',');
 
     const content =
         '<!DOCTYPE html>\n' +
         '<html>\n' +
         '<head>\n' +
         '  <meta charset="UTF-8">\n' +
-        '  <title>PDF</title>\n' +
+        '  <title>' + name + '</title>\n' +
         '  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>\n' +
         '</head>\n' +
         '<body>\n' +
         '  <div><canvas id="pdf" width="400" height="150"></canvas><\div>\n' +
         '  <script>\n' +
-        '    const labels = [' + data.map(d => `"${Math.floor(d.x*n)/n}"`).join(',') + '];\n' +
+        '    const labels = [' + labelsStr + '];\n' +
         '    const data = {\n' +
         '      labels: labels,\n' +
         '      datasets: [{\n' +
         '        backgroundColor: "rgb(255, 99, 132)",\n' +
         '        borderColor: "rgb(255, 99, 132)",\n' +
-        '        data: [' + data.map(d => d.y.toString()).join(',') + ']\n' +
+        '        data: [' + dataStr + ']\n' +
         '      }]\n' +
         '    };\n' +
         '    \n' +
@@ -44,8 +45,11 @@ function outputPdf(file: string, dist: Distribution,
         '      data,\n' +
         '      options: {\n' +
         '        scales: {\n' +
-        '          y: { beginAtZero: true }\n' +
-        '        }\n' +
+        '          y: {\n' +
+        '            suggestedMin: ' + yMin + ',\n' +
+        '            suggestedMax: ' + yMax + '\n' +
+        '          }\n' +
+        '        },\n' +
         '      }\n' +
         '    };\n' +
         '    \n' +
